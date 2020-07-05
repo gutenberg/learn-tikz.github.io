ebff13cd955baac35b88397de835dc85464c1178
David Carlisle <d.p.carlisle@gmail.com>
2020-07-05 23:59:35 +0100

    change and localise Loading..... flash text

---
+++ vi/includes/buttons.js
@@ -0,0 +1,8 @@
+var buttons ={
+    "edit":             "sửa",
+    "copy":             "sao chép",
+    "Open in Overleaf": "Mở trong Overleaf",
+    "LaTeX Online":     "Chạy với LaTeX Online",
+    "Delete Output":    "Xóa output",
+    "Compiling PDF":    "Compiling PDF"
+}
